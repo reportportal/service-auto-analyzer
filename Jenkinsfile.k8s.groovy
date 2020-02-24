828aa4c3fa4837b3384059d16c9c2f08a10ed88c
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-02-24 17:59:55 +0300

    Adding Jenkinsfile.k8s.groovy

---
+++ Jenkinsfile.k8s.groovy
@@ -0,0 +1,97 @@
+#!groovy
+
+//String podTemplateConcat = "${serviceName}-${buildNumber}-${uuid}"
+def label = "worker-${env.JOB_NAME}-${UUID.randomUUID().toString()}"
+println("Worker name: ${label}")
+
+podTemplate(
+        label: "${label}",
+        containers: [
+                containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:alpine'),
+                containerTemplate(name: 'docker', image: 'docker:dind', ttyEnabled: true, alwaysPullImage: true, privileged: true,
+                        command: 'dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --storage-driver=overlay'),
+                containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.8', command: 'cat', ttyEnabled: true),
+                containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:v3.0.2', command: 'cat', ttyEnabled: true,
+                        resourceRequestCpu: '300m',
+                        resourceLimitCpu: '500m',
+                        resourceRequestMemory: '128Mi',
+                        resourceLimitMemory: '256Mi'),
+                // containerTemplate(name: 'yq', image: 'mikefarah/yq', command: 'cat', ttyEnabled: true),
+                containerTemplate(name: 'httpie', image: 'blacktop/httpie', command: 'cat', ttyEnabled: true)
+        ],
+        volumes: [
+                emptyDirVolume(memory: false, mountPath: '/var/lib/docker'),
+                secretVolume(mountPath: '/etc/.dockercreds', secretName: 'docker-creds'),
+                hostPathVolume(mountPath: '/go/pkg/mod', hostPath: '/tmp/jenkins/go')
+        ]
+) {
+
+    node("${label}") {
+        def srvRepo = "quay.io/reportportal/service-auto-analyzer"
+        def srvVersion = "BUILD-${env.BUILD_NUMBER}"
+        def tag = "$srvRepo:$srvVersion"
+
+        /**
+         * General ReportPortal Kubernetes Configuration and Helm Chart
+         */
+        def k8sDir = "kubernetes"
+        def k8sChartDir = "$k8sDir/reportportal/v5"
+
+        /**
+         * Jenkins utilities and environment Specific k8s configuration
+         */
+        def ciDir = "reportportal-ci"
+        def appDir = "app"
+
+        parallel 'Checkout Infra': {
+            stage('Checkout Infra') {
+                sh 'mkdir -p ~/.ssh'
+                sh 'ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts'
+                sh 'ssh-keyscan -t rsa git.epam.com >> ~/.ssh/known_hosts'
+                dir('kubernetes') {
+                    git branch: "master", url: 'https://github.com/reportportal/kubernetes.git'
+
+                }
+                dir('reportportal-ci') {
+                    git credentialsId: 'epm-gitlab-key', branch: "master", url: 'git@git.epam.com:epmc-tst/reportportal-ci.git'
+                }
+
+            }
+        }, 'Checkout Service': {
+            stage('Checkout Service') {
+                dir('app') {
+                    checkout scm
+                }
+            }
+        }
+        def utils = load "${ciDir}/jenkins/scripts/util.groovy"
+        def helm = load "${ciDir}/jenkins/scripts/helm.groovy"
+        def docker = load "${ciDir}/jenkins/scripts/docker.groovy"
+
+        docker.init()
+        helm.init()
+
+
+        utils.scheduleRepoPoll()
+
+        dir('app') {
+            container('docker') {
+                stage('Build Image') {
+                    sh "docker build -t $tag --build-arg version=`cat VERSION`-${env.BUILD_NUMBER} --build-arg prod=false --label quay.expires-after=1w -f Dockerfile ."
+                }
+                stage('Push Image') {
+                    sh "docker push $tag"
+                }
+            }
+        }
+
+        stage('Deploy to Dev') {
+            helm.deploy("./$k8sChartDir", ["serviceanalyzer.repository": srvRepo, "serviceanalyzer.tag": srvVersion], false) // with wait
+        }
+
+        stage('DVT Test') {
+            helm.testDeployment("reportportal", "reportportal-analyzer", "$srvVersion")
+        }
+
+    }
+}

900d8cce4351f6bf9d5f31a4cdabe845a5d5a15b
Mariya Ivanina <mashunya-1604@yandex.by>
2020-06-19 12:19:02 +0300

    fixed security issues with passwords in url

--- app.py
+++ app.py
@@ -32,11 +32,9 @@ from utils import utils
 
 
 APP_CONFIG = {
-    # "esHost":            os.getenv("ES_HOSTS", "http://elasticsearch:9200"),
-    "esHost":            os.getenv("ES_HOSTS", "http://localhost:9200"),
+    "esHost":            os.getenv("ES_HOSTS", "http://elasticsearch:9200"),
     "logLevel":          os.getenv("LOGGING_LEVEL", "DEBUG"),
-    # "amqpUrl":           os.getenv("AMQP_URL", "amqp://rabbitmq:rabbitmq@rabbitmq:5672"),
-    "amqpUrl":           os.getenv("AMQP_URL", "amqp://rabbitmq:rabbitmq@localhost:5672"),
+    "amqpUrl":           os.getenv("AMQP_URL", ""),
     "exchangeName":      os.getenv("AMQP_EXCHANGE_NAME", "analyzer"),
     "analyzerPriority":  int(os.getenv("ANALYZER_PRIORITY", "1")),
     "analyzerIndex":     json.loads(os.getenv("ANALYZER_INDEX", "true").lower()),

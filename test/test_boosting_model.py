778526b96c96b9c1a75af26c21bbffc7836c7165
Mariya Ivanina <mashunya-1604@yandex.by>
2020-09-11 14:32:30 +0300

    updated the boosting models

--- test/test_boosting_model.py
+++ test/test_boosting_model.py
@@ -62,7 +62,8 @@ class TestBoostingModel(unittest.TestCase):
             "min_word_length":  0,
             "filter_min_should_match": filter_fields,
             "filter_min_should_match_any": filter_fields_any,
-            "number_of_log_lines": number_of_log_lines
+            "number_of_log_lines": number_of_log_lines,
+            "chosen_namespaces": {"tv.horizon":25, "sun.reflect": 10}
         }
 
     @utils.ignore_warnings

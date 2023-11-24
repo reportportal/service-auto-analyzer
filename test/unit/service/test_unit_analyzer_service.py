d7c3f62375a33b567e389bfe269f9952f62571bf
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-11-25 01:35:44 +0300

    EPMRPP-87957: fix tests

--- test/unit/service/test_unit_analyzer_service.py
+++ test/unit/service/test_unit_analyzer_service.py
@@ -34,29 +34,28 @@ def get_empty_bool_query():
     }
 
 
-DEFAULT_LAUNCH_NAME_SEARCH = {'must': [{'term': {'launch_name': {'value': DEFAULT_LAUNCH_NAME}}}], 'should': []}
+DEFAULT_LAUNCH_NAME_SEARCH = {'must': [{'term': {'launch_name': {'value': DEFAULT_LAUNCH_NAME}}}],
+                              'should': [{'term': {'launch_id': {'value': DEFAULT_LAUNCH_ID,
+                                                                 'boost': DEFAULT_BOOST_LAUNCH}}}]}
 
 
 @pytest.mark.parametrize(
-    'launch_number, launch_mode, expected_query',
+    'previous_launch_id, launch_mode, expected_query',
     [
         (2, 'LAUNCH_NAME', DEFAULT_LAUNCH_NAME_SEARCH),
         (2, 'CURRENT_AND_THE_SAME_NAME', DEFAULT_LAUNCH_NAME_SEARCH),
         (2, 'CURRENT_LAUNCH', {'must': [{'term': {'launch_id': {'value': DEFAULT_LAUNCH_ID}}}], 'should': []}),
-        (2, 'PREVIOUS_LAUNCH', {'must': [{'term': {'launch_number': {'value': 1}}},
-                                         {'term': {'launch_name': {'value': DEFAULT_LAUNCH_NAME}}}], 'should': []}),
-        (None, 'PREVIOUS_LAUNCH', {'must': [{'term': {'launch_number': {'value': -1}}},
-                                            {'term': {'launch_name': {'value': DEFAULT_LAUNCH_NAME}}}], 'should': []}),
-        ('3', 'PREVIOUS_LAUNCH', {'must': [{'term': {'launch_number': {'value': 2}}},
-                                           {'term': {'launch_name': {'value': DEFAULT_LAUNCH_NAME}}}], 'should': []}),
+        (2, 'PREVIOUS_LAUNCH', {'must': [{'term': {'launch_id': {'value': 2}}}], 'should': []}),
+        (None, 'PREVIOUS_LAUNCH', {'must': [], 'should': []}),
+        ('3', 'PREVIOUS_LAUNCH', {'must': [{'term': {'launch_id': {'value': 3}}},], 'should': []}),
         (2, None, {'must': [], 'should': [{'term': {'launch_name': {'value': DEFAULT_LAUNCH_NAME,
                                                                     'boost': DEFAULT_BOOST_LAUNCH}}}]})
     ]
 )
-def test_add_constraints_for_launches_into_query(launch_number, launch_mode, expected_query):
+def test_add_constraints_for_launches_into_query(previous_launch_id, launch_mode, expected_query):
     launch = mock.Mock()
     launch.launchId = DEFAULT_LAUNCH_ID
-    launch.launchNumber = launch_number
+    launch.previousLaunchId = previous_launch_id
     launch.launchName = DEFAULT_LAUNCH_NAME
     launch.analyzerConfig.analyzerMode = launch_mode
     analyzer = AnalyzerService(None, DEFAULT_SEARCH_CONFIG)

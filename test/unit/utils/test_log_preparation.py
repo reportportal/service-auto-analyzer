9c57dbf4a9fc9b06f438a031e5625b235890e5fa
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-06-11 12:13:22 +0300

    Add text separators tests

--- test/unit/utils/test_log_preparation.py
+++ test/unit/utils/test_log_preparation.py
@@ -12,12 +12,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from test import read_file_lines
+from test import read_file_lines, read_file
 from app.utils import log_preparation
 
 
-def test_remove_starting_thread_namer():
+def test_remove_starting_thread_name():
     log = read_file_lines('test_res/test_logs', 'log_line_timestamps.txt')
     expected_log = read_file_lines('test_res/test_logs', 'log_line_prepared.txt')
     for i, line in enumerate(log):
         assert log_preparation.basic_prepare(line) == expected_log[i].strip()
+
+
+def test_separators_log_prepare():
+    log = read_file('test_res/test_logs/separators', 'mixed_markdown_separators.txt')
+    expected_log = read_file('test_res/test_logs/separators', 'mixed_markdown_separators_prepared.txt')
+    assert log_preparation.basic_prepare(log) == expected_log.strip()

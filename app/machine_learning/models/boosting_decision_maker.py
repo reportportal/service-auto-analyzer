1aad26b765897ba37c10ad18c209f22e1570ec0c
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-07-17 17:40:07 +0300

    Analysis model training refactoring: WIP

--- app/machine_learning/models/boosting_decision_maker.py
+++ app/machine_learning/models/boosting_decision_maker.py
@@ -22,7 +22,7 @@ from app.machine_learning import feature_encoder
 from app.machine_learning.models import MlModel
 from app.utils import text_processing
 
-logger = logging.getLogger("analyzerApp.boosting_decision_maker")
+LOGGER = logging.getLogger("analyzerApp.boosting_decision_maker")
 
 MODEL_FILES: list[str] = ['boost_model.pickle', 'data_features_config.pickle',
                           'features_dict_with_saved_objects.pickle']
@@ -33,13 +33,14 @@ class BoostingDecisionMaker(MlModel):
     full_config: dict[str, Any]
     feature_ids: list[int]
     monotonous_features: list[int]
+    boost_model: Any
 
     def __init__(self, object_saver: ObjectSaver, tags: str = 'global boosting model', *, n_estimators: int = 75,
                  max_depth: int = 5, monotonous_features: str = '') -> None:
         super().__init__(object_saver, tags)
         self.n_estimators = n_estimators
         self.max_depth = max_depth
-        self.xg_boost = XGBClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=43)
+        self.boost_model = XGBClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=43)
         self.full_config = {}
         self.feature_ids = []
         self.monotonous_features = text_processing.transform_string_feature_range_into_list(
@@ -91,13 +92,13 @@ class BoostingDecisionMaker(MlModel):
         if self.loaded:
             return
         boost_model, features_config, features_dict = self._load_models(MODEL_FILES)
-        self.n_estimators, self.max_depth, self.xg_boost = boost_model
+        self.n_estimators, self.max_depth, self.boost_model = boost_model
         self.full_config, self.feature_ids, self.monotonous_features = features_config
         self.features_dict_with_saved_objects = self.transform_feature_encoders_to_objects(features_dict)
         self._loaded = True
 
     def save_model(self):
-        self._save_models(zip(MODEL_FILES, [[self.n_estimators, self.max_depth, self.xg_boost],
+        self._save_models(zip(MODEL_FILES, [[self.n_estimators, self.max_depth, self.boost_model],
                                             [self.full_config, self.feature_ids, self.monotonous_features],
                                             self.transform_feature_encoders_to_dict()]))
 
@@ -105,23 +106,23 @@ class BoostingDecisionMaker(MlModel):
         mon_features = [
             (1 if feature in self.monotonous_features else 0) for feature in self.get_feature_ids()]
         mon_features_prepared = "(" + ",".join([str(f) for f in mon_features]) + ")"
-        self.xg_boost = XGBClassifier(n_estimators=self.n_estimators,
-                                      max_depth=self.max_depth, random_state=43,
-                                      monotone_constraints=mon_features_prepared)
-        self.xg_boost.fit(train_data, labels)
+        self.boost_model = XGBClassifier(
+            n_estimators=self.n_estimators, max_depth=self.max_depth, random_state=43,
+            monotone_constraints=mon_features_prepared)
+        self.boost_model.fit(train_data, labels)
         self._loaded = True
-        logger.info("Train score: %s", self.xg_boost.score(train_data, labels))
-        logger.info("Feature importances: %s", self.xg_boost.feature_importances_)
+        LOGGER.info("Train score: %s", self.boost_model.score(train_data, labels))
+        LOGGER.info("Feature importances: %s", self.boost_model.feature_importances_)
 
     def validate_model(self, valid_test_set, valid_test_labels):
         res, res_prob = self.predict(valid_test_set)
-        f1_score = self.xg_boost.score(valid_test_set, valid_test_labels)
-        logger.info("Valid dataset F1 score: %s", f1_score)
-        logger.info(confusion_matrix(valid_test_labels, res))
-        logger.info(classification_report(valid_test_labels, res))
+        f1_score = self.boost_model.score(valid_test_set, valid_test_labels)
+        LOGGER.info("Valid dataset F1 score: %s", f1_score)
+        LOGGER.info(confusion_matrix(valid_test_labels, res))
+        LOGGER.info(classification_report(valid_test_labels, res))
         return f1_score
 
     def predict(self, data: list):
         if not len(data):
             return [], []
-        return self.xg_boost.predict(data), self.xg_boost.predict_proba(data)
+        return self.boost_model.predict(data), self.boost_model.predict_proba(data)

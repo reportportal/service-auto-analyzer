6e87ade020aa17f6b4ba01258181aa83ec629292
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-04-16 18:23:54 +0300

    Fix URL sanitizing pattern

--- app/utils/text_processing.py
+++ app/utils/text_processing.py
@@ -718,7 +718,7 @@ def build_url(main_url: str, url_params: list) -> str:
 
 def remove_credentials_from_url(url):
     parsed_url = urlparse(url)
-    new_netloc = re.sub("^[^:]:[^@]@", "", parsed_url.netloc)
+    new_netloc = re.sub("^[^:]+:[^@]*@", "", parsed_url.netloc)
     return url.replace(parsed_url.netloc, new_netloc)
 
 

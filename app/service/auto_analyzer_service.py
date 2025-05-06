2b2b53642edd8fa59e8ebf0f4a4e676c78623d63
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-05-06 17:30:02 +0300

    Update logging

--- app/service/auto_analyzer_service.py
+++ app/service/auto_analyzer_service.py
@@ -468,23 +468,21 @@ class AutoAnalyzerService(AnalyzerService):
                             scores_by_issue_type = boosting_data_gatherer.find_most_relevant_by_type()
 
                             for i in range(len(issue_type_names)):
+                                issue_type_name = issue_type_names[i]
+                                log_id = scores_by_issue_type[issue_type_name]["mrHit"]["_id"]
                                 logger.debug(
-                                    "Most relevant item with issue type %s has id %s",
-                                    issue_type_names[i],
-                                    boosting_data_gatherer
-                                    .find_most_relevant_by_type()[issue_type_names[i]]["mrHit"]["_id"])
+                                    f"Most relevant item with issue type '{issue_type_name}' has log id: {log_id}")
+                                label = predicted_labels[i]
+                                probability = round(predicted_labels_probability[i][1], 3)
                                 logger.debug(
-                                    "Issue type %s has label %d and probability %.3f for features %s",
-                                    issue_type_names[i],
-                                    predicted_labels[i],
-                                    predicted_labels_probability[i][1],
-                                    feature_data[i])
+                                    f"Issue type '{issue_type_name}' has label '{label}' and probability {probability} "
+                                    f"with calculated feature values: {feature_data[i]}")
 
                             predicted_issue_type, prob, global_idx = utils.choose_issue_type(
                                 predicted_labels,
                                 predicted_labels_probability,
                                 issue_type_names,
-                                boosting_data_gatherer.find_most_relevant_by_type())
+                                scores_by_issue_type)
 
                             if predicted_issue_type:
                                 chosen_type = scores_by_issue_type[predicted_issue_type]

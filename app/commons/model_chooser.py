5aafcb173a7e7bf65bc84d28ac024d5b684cee72
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-06-25 17:37:49 +0300

    Refactor `ModelChooser.choose_model`

--- app/commons/model_chooser.py
+++ app/commons/model_chooser.py
@@ -89,25 +89,26 @@ class ModelChooser:
         use_for_hash = str(hash_source if hash_source is not None else project_id).encode("utf-8")
         hash_code = hashlib.md5(use_for_hash).digest()
         test_value = int.from_bytes(hash_code) % 100
-        model = self.global_models[model_type]
-        if test_value > int(custom_model_prob * 100):  # use hash instead of random to ensure in repeatable results
-            logger.debug(
-                f"Using global model of type '{model_type.name}', for project {project_id}, "
-                f"for hash source {str(hash_source)}."
-            )
-            return model
+        if test_value <= int(custom_model_prob * 100):
+            folders = self.object_saver.get_folder_objects(f"{model_type.name}_model/", project_id)
+            if len(folders):
+                try:
+                    model = CUSTOM_MODEL_MAPPING[model_type](
+                        object_saving.create(self.app_config, project_id, folders[0])
+                    )
+                    model.load_model()
+                    logger.debug(
+                        f"Using custom model of type '{model_type.name}', for project '{project_id}', "
+                        f"for hash source '{str(hash_source)}'."
+                    )
+                    return model
+                except Exception as err:
+                    logger.exception(f"Unable to load model of type: {model_type.name}", exc_info=err)
         logger.debug(
-            f"Using custom model of type '{model_type.name}', for project {project_id}, "
-            f"for hash source {str(hash_source)}."
+            f"Using global model of type '{model_type.name}', for project '{project_id}', "
+            f"for hash source '{str(hash_source)}'."
         )
-        folders = self.object_saver.get_folder_objects(f"{model_type.name}_model/", project_id)
-        if len(folders):
-            try:
-                model = CUSTOM_MODEL_MAPPING[model_type](object_saving.create(self.app_config, project_id, folders[0]))
-                model.load_model()
-            except Exception as err:
-                logger.exception(err)
-        return model
+        return self.global_models[model_type]
 
     def delete_old_model(self, model_type: ModelType, project_id: str | int | None = None):
         all_folders = self.object_saver.get_folder_objects(f"{model_type.name}_model/", project_id)

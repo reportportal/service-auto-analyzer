c3b5d05ccc7f220e32f1040654e03a1e32a9620e
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2026-06-25 14:48:55 +0300

    Remove results publishing

--- app/service/index_service.py
+++ app/service/index_service.py
@@ -74,7 +74,7 @@ class IndexService:
                 test_item_ids.append(str(test_item.testItemId))
         return test_item_ids, bodies
 
-    def index_logs(self, launches: list[Launch]) -> BulkResponse:
+    def index_logs(self, launches: list[Launch]) -> None:
         """Index launches to the index with project name"""
         launch_ids = {str(launch_obj.launchId) for launch_obj in launches}
         launch_ids_str = ", ".join(launch_ids)
@@ -82,15 +82,10 @@ class IndexService:
         LOGGER.info(f"Indexing {len(launch_ids)} launches of project '{project}': {launch_ids_str}")
         t_start = time()
         test_item_queue = self._to_launch_test_item_list(launches)
-        if project is None:
-            return BulkResponse(took=0, errors=False)
-
         project_with_prefix = text_processing.unite_project_name(project, self.app_config.esProjectIndexPrefix)
         self.es_client.create_index_if_not_exists(project_with_prefix)
         test_item_ids, bodies = self._to_index_bodies(project_with_prefix, test_item_queue)
-        logs_with_exceptions = utils.extract_all_exceptions(bodies)
-        result = self.es_client.bulk_index(bodies)
-        result.logResults = logs_with_exceptions
+        self.es_client.bulk_index(bodies)
         num_logs_with_defect_types = self.es_client.merge_logs(test_item_ids, project_with_prefix)
 
         if self.app_config.amqpUrl and not self.app_config.disableTrain:
@@ -108,7 +103,6 @@ class IndexService:
             f"Indexing {len(launch_ids)} launches of project '{project}' finished: {launch_ids_str}. "
             f"It took {time_passed} sec."
         )
-        return result
 
     def send_stats_info(self, stats_info: dict) -> None:
         LOGGER.info("Started sending stats about analysis")
